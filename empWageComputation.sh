92a82269ddb7a4c879421125deb3e7eb18d6e6d6
ShubhamKulkarni14 <shubhuk14@gmail.com>
2020-11-22 19:27:26 +0530

    Added :Calculation Of Employee Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -x
 
 isPresent=1
+
 random=$(( RANDOM%2 ))
 echo "Random number: "$random
 
 if(( $random == $isPresent ))
 then
 	echo "Employee is Present!"
+		empHrs=8
+		ratePerHour=20
+	salary=$(( $empHrs * $ratePerHour))
 else
 	echo "Employee is absent!"
+		salary=0
 fi
+
+echo "Salary: $salary"
