815d1674a5772219d6b02092922edadef04cbd86
ShubhamKulkarni14 <shubhuk14@gmail.com>
2020-11-22 19:32:32 +0530

    Added :Part Time Employee Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -x
 
-isPresent=1
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HR=20
 
-random=$(( RANDOM%2 ))
-echo "Random number: "$random
+random=$(( RANDOM%3 ))
+echo "Random number: " $random
 
-if(( $random == $isPresent ))
+if(( $random == $IS_PART_TIME ))
 then
-	echo "Employee is Present!"
-		empHrs=8
-		ratePerHour=20
-	salary=$(( $empHrs * $ratePerHour))
+	empHrs=4
+elif(( $random == $IS_FULL_TIME))
+then
+	empHrs=8
 else
-	echo "Employee is absent!"
-		salary=0
+	empHrs=0
 fi
+echo "Employee hours: " $empHrs
+salary=$(( $empHrs * $ EMP_RATE_PER_HR ))
 
 echo "Salary: $salary"
